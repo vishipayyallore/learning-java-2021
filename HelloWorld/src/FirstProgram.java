0390d91c65de1b17a57c8c48edfd9296033134aa
Viswanatha Swamy <vishipayyallore@yahoo.com>
2020-11-22 00:43:53 +0530

    Hello World

---
+++ HelloWorld/src/FirstProgram.java
@@ -0,0 +1,11 @@
+public class FirstProgram {
+
+    public static void main(String[] args){
+
+        String userName = "Shiva Sai";
+        int userAge = 21;
+
+        System.out.println( String.format("Name: %s, Age: %d", userName, userAge) );
+    }
+
+}
