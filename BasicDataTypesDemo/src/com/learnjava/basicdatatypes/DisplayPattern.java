b034437c68f09ee2f9ba1978c85e2e654c885a45
Viswanatha Swamy <vishipayyallore@yahoo.com>
2020-12-03 00:30:07 +0530

    Few Changes

--- BasicDataTypesDemo/src/com/learnjava/basicdatatypes/DisplayPattern.java
+++ BasicDataTypesDemo/src/com/learnjava/basicdatatypes/DisplayPattern.java
@@ -1,4 +1,27 @@
 package com.learnjava.basicdatatypes;
 
 public class DisplayPattern {
+
+    static final String DEFAULT_PATTERN = " ";
+
+    public static boolean displayHeader(String title){
+
+        int patternLength = 100;
+        int leftPadding = (patternLength - title.length())/2;
+
+        System.out.println(repeatPattern(patternLength, "="));
+        System.out.println( String.format("%s%s", repeatPattern(leftPadding), title));
+        System.out.println(repeatPattern(patternLength, "-"));
+
+        return true;
+    }
+
+    public static String repeatPattern(int length, String pattern) {
+        return new String(new char[length]).replace("\0", pattern);
+    }
+
+    public static String repeatPattern(int length) {
+        return new String(new char[length]).replace("\0", DEFAULT_PATTERN);
+    }
+
 }
--- BasicDataTypesDemo/src/com/learnjava/basicdatatypes/Main.java
+++ BasicDataTypesDemo/src/com/learnjava/basicdatatypes/Main.java
@@ -2,35 +2,74 @@ package com.learnjava.basicdatatypes;
 
 public class Main {
 
-    static final String DEFAULT_PATTERN = " ";
+    static final char DEFAULT_SPACE = ' ';
+    static final char NULL_CHARACTER = '\0';
+    static final char DEFAULT_TOP_PATTERN = '=';
+    static final char DEFAULT_BOTTOM_PATTERN = '-';
+    static final char DEFAULT_FOOTER_PATTERN = '*';
+
+    public static final String TEXT_RESET = "\u001B[0m";
+    public static final String TEXT_BLUE = "\u001B[34m";
+    public static final String ANSI_PURPLE = "\u001B[35m";
+
+    /*
+    public static final String ANSI_RESET = "\u001B[0m";
+    public static final String ANSI_BLACK = "\u001B[30m";
+    public static final String ANSI_RED = "\u001B[31m";
+    public static final String ANSI_GREEN = "\u001B[32m";
+    public static final String ANSI_YELLOW = "\u001B[33m";
+    public static final String ANSI_BLUE = "\u001B[34m";
+    public static final String ANSI_PURPLE = "\u001B[35m";
+    public static final String ANSI_CYAN = "\u001B[36m";
+    public static final String ANSI_WHITE = "\u001B[37m";
+    */
 
     public static void main(String[] args) {
 
         ShowNumericLimits showNumericLimits = new ShowNumericLimits();
 
-        displayHeader("Basic Numeric Type Limits");
+        System.out.println(TEXT_BLUE + "This text is red!" + TEXT_RESET);
 
+        displayHeader("Basic Numeric Type Limits", DEFAULT_TOP_PATTERN, DEFAULT_BOTTOM_PATTERN);
         showNumericLimits.showDetails();
+        System.out.println( String.format("%s%s%s", ANSI_PURPLE, repeatPattern(100, DEFAULT_FOOTER_PATTERN), TEXT_RESET ) );
+        System.out.println();
+
+        showPrimitiveDataTypesDemo();
+
+        // Few Important Points
+        System.out.println( String.format("Integer.MIN_VALUE/2 === %d", (Integer.MIN_VALUE/2)));
+        System.out.println( String.format("Short.MIN_VALUE/2 === %d", (Short.MIN_VALUE/2)));
+        System.out.println( String.format("Byte.MIN_VALUE/2 === %d", (Byte.MIN_VALUE/2)));
     }
 
-    public static boolean displayHeader(String title){
+    private static void showPrimitiveDataTypesDemo() {
+        byte byteValue = 10;
+        short shortValue = 20;
+        int intValue = 50;
+
+        long longValue = 50000L + 10L * (byteValue + shortValue + intValue);
+        System.out.println(String.format("Long Value: %d", longValue));
+    }
+
+    public static boolean displayHeader(String title, char topPattern, char bottomPattern){
 
         int patternLength = 100;
         int leftPadding = (patternLength - title.length())/2;
 
-        System.out.println(repeatPattern(patternLength, "="));
-        System.out.println( String.format("%s%s", repeatPattern(leftPadding), title));
-        System.out.println(repeatPattern(patternLength, "-"));
+        System.out.println( String.format("%s%s%s", ANSI_PURPLE,repeatPattern(patternLength, topPattern), TEXT_RESET));
+        System.out.println( String.format("%s%s%s%s", ANSI_PURPLE,repeatPattern(leftPadding), title, TEXT_RESET));
+        System.out.println( String.format("%s%s%s", ANSI_PURPLE,repeatPattern(patternLength, bottomPattern), TEXT_RESET));
 
         return true;
     }
 
-    public static String repeatPattern(int length, String pattern) {
-        return new String(new char[length]).replace("\0", pattern);
+    public static String repeatPattern(int length, char pattern) {
+        return new String(new char[length]).replace(NULL_CHARACTER, pattern);
     }
 
     public static String repeatPattern(int length) {
-        return new String(new char[length]).replace("\0", DEFAULT_PATTERN);
+        return new String(new char[length]).replace(NULL_CHARACTER, DEFAULT_SPACE);
     }
 
 }
