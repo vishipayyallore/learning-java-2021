b70338c2c719553d75d72a6c659690bab5e15b30
Viswanatha Swamy <vishipayyallore@yahoo.com>
2021-02-17 22:06:26 +0530

    Update Hello.java

--- 1HelloWorld/src/Hello.java
+++ 1HelloWorld/src/Hello.java
@@ -2,7 +2,11 @@ public class Hello {
 
     public static void main(String[] args){
 
+        // Variables.
+        int age = 21;
+
         System.out.println("Hello, Shiva");
+        System.out.println(age);
     }
 
 }
